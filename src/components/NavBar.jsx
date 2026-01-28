67b0468a78ac2e9c06362f450169f818b5070175
CamilaNevesLinder <camilaneveslindner@.com>
2026-01-28 15:16:44 -0300

    add nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <div className="flex  justify-center bg-transparent p-8">
+      <div className="fixed left-5 top-5">
+        <img src="public/img/icons8-cardápio-50.png" alt="" />
+      </div>
+
+      <h1 className="font-bold text-2xl  ">Shine Rings</h1>
+    </div>
+  );
+}
+
+export default NavBar;

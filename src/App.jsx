bc713202293f1b6dbb2e27b86a87c7ba75c91b82
CamilaNevesLinder <camilaneveslindner@.com>
2026-01-29 16:37:17 -0300

    add intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import Intro from "./components/Intro";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <Intro />
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,11 @@
 function NavBar() {
   return (
-    <div className="flex  justify-center bg-transparent p-8 ">
+    <div className="flex  justify-center  p-6 z-50  fixed top-0 w-full bg-white/10 backdrop-blur-sm  ">
       <div className="fixed left-5 top-5">
         <img src="public/img/icons8-cardápio-50.png" alt="" />
       </div>
 
-      <h1 className="font-bold text-2xl  ">Shine Rings</h1>
+      <h1 className="font-bold text-2xl  text-white ">Shine Rings</h1>
 
       <div className="w-13 fixed right-5 top-5">
         <img src="public/img/magnifying-glass.png" alt="" />
